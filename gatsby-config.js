fb9be4bec0c76dbc9bdfecced4f059cf544a7b2e
hunterbecton <hunter@hunterbecton.com>
2021-02-08 12:52:26 -0500

    Removed robots plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,26 +61,5 @@ module.exports = {
         icon: `src/images/favicon.svg`,
       },
     },
-    {
-      resolve: "gatsby-plugin-robots-txt",
-      options: {
-        resolveEnv: () => NETLIFY_ENV,
-        env: {
-          production: {
-            policy: [{ userAgent: "*" }],
-          },
-          "branch-deploy": {
-            policy: [{ userAgent: "*", disallow: ["/"] }],
-            sitemap: null,
-            host: null,
-          },
-          "deploy-preview": {
-            policy: [{ userAgent: "*", disallow: ["/"] }],
-            sitemap: null,
-            host: null,
-          },
-        },
-      },
-    },
   ],
 }
